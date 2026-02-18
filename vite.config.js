02a583846c77907e3317ef5b2b7b69d094a5e15c
xdmset <0322103691@ut-tijuana.edu.mx>
2026-02-17 23:31:21 -0800

    icons

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'icons/web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icons/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
